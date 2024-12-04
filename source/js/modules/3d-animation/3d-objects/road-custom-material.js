17a4559b8d8672432222aaebfa6db0e6077dff4a
Domore <placebo9132@gmail.com>
2024-12-04 13:54:32 +0500

    6.35. Использование кастомного материала ShaderMaterial для процедурных текстур «Коврика» и «Дороги»

---
+++ source/js/modules/3d-animation/3d-objects/road-custom-material.js
@@ -0,0 +1,33 @@
+import * as THREE from "three";
+import { color3D } from "../../../helpers/3d-data";
+export class RoadCustomMaterial extends THREE.MeshStandardMaterial {
+  onBeforeCompile(shader) {
+    shader.uniforms = {
+      ...shader.uniforms,
+      roadColor: new THREE.Uniform(new THREE.Color(color3D.Grey)),
+      stripesColor: new THREE.Uniform(new THREE.Color(color3D.White)),
+    };
+    shader.vertexShader = shader.vertexShader.replace(
+      `#include <uv_pars_vertex>`,
+      `varying vec2 vUv;`
+    );
+    shader.vertexShader = shader.vertexShader.replace(
+      `#include <uv_vertex>`,
+      `vUv = uv;`
+    );
+    shader.fragmentShader = shader.fragmentShader.replace(
+      `varying vec3 vViewPosition;`,
+      `varying vec3 vViewPosition;
+        varying vec2 vUv;
+        uniform vec3 roadColor;
+        uniform vec3 stripesColor;`
+    );
+    shader.fragmentShader = shader.fragmentShader.replace(
+      `#include <map_fragment>`,
+      `float strength = step(0.86, mod(vUv.y + 0.4, 1.0));
+      strength *= step(0.6, mod(vUv.x * 4.0 + 0.4, 1.0));
+      vec3 color = strength < 0.5 ? roadColor : stripesColor;
+      diffuseColor = vec4(color, vUv);`
+    );
+  }
+}
