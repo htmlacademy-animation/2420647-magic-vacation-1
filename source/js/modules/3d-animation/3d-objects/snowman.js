cb46a976e502deb980fc9c5caf916069aa830943
Domore <placebo9132@gmail.com>
2024-12-04 12:07:53 +0500

    6.34. Добавление однотонных материалов личного проекта для ранее созданных объектов

--- source/js/modules/3d-animation/3d-objects/snowman.js
+++ source/js/modules/3d-animation/3d-objects/snowman.js
@@ -1,7 +1,13 @@
 import * as THREE from "three";
 export class Snowman extends THREE.Group {
-  constructor() {
+  constructor(options) {
     super();
+    this.colorSphere = options.colorSphere;
+    this.metalnessSphere = options.metalnessSphere;
+    this.roughnessSphere = options.roughnessSphere;
+    this.colorCone = options.colorCone;
+    this.metalnessCone = options.metalnessCone;
+    this.roughnessCone = options.roughnessCone;
     this.constructChildren();
   }
   constructChildren() {
@@ -11,10 +17,10 @@ export class Snowman extends THREE.Group {
   }
   addTopSphere() {
     const material = new THREE.MeshStandardMaterial({
-      color: 0xffffff,
-      metalness: 0.05,
+      color: new THREE.Color(this.colorSphere),
+      metalness: this.metalnessSphere,
+      roughness: this.roughnessSphere,
       emissive: 0x243452,
-      roughness: 0.7,
     });
     const geometry = new THREE.SphereGeometry(44, 30, 30);
     const mesh = new THREE.Mesh(geometry, material);
@@ -22,10 +28,10 @@ export class Snowman extends THREE.Group {
   }
   addBottomSphere() {
     const material = new THREE.MeshStandardMaterial({
-      color: 0xffffff,
-      metalness: 0.05,
+      color: new THREE.Color(this.colorSphere),
+      metalness: this.metalnessSphere,
+      roughness: this.roughnessSphere,
       emissive: 0x243452,
-      roughness: 0.7,
     });
     const geometry = new THREE.SphereGeometry(78, 30, 30);
     const mesh = new THREE.Mesh(geometry, material);
@@ -34,10 +40,9 @@ export class Snowman extends THREE.Group {
   }
   addCone() {
     const material = new THREE.MeshStandardMaterial({
-      color: 0xfe4601,
-      metalness: 0.05,
-      emissive: 0x000000,
-      roughness: 0.7,
+      color: new THREE.Color(this.colorCone),
+      metalness: this.metalnessCone,
+      roughness: this.roughnessCone,
     });
     const geometry = new THREE.ConeGeometry(18, 75, 30);
     const mesh = new THREE.Mesh(geometry, material);
